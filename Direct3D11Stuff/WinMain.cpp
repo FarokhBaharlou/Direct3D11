a1c1b2133b795e66ebf2e1a5573409d71ab6f471
Farokh <farokhbahalou@yahoo.com>
2021-11-17 22:44:12 -0500

    Message loop and basic window procedure.

--- Direct3D11Stuff/WinMain.cpp
+++ Direct3D11Stuff/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(93);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
@@ -10,7 +21,7 @@ int CALLBACK WinMain(HINSTANCE hInstance,
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -34,7 +45,17 @@ int CALLBACK WinMain(HINSTANCE hInstance,
 	// Show window
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	// Message pump
+	MSG msg;
+	BOOL getResult;
+	while ((getResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-	return 0;
+	if (getResult == -1)
+		return -1;
+	else
+		return msg.wParam;
 }
