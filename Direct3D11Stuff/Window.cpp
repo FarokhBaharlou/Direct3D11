180e38617993cefc154b6edcab07c0fc420e36ff
Farokh <farokhbahalou@yahoo.com>
2021-12-01 23:30:23 -0500

    Added Keyboard class to encapsulate handling keyboard events on windows side.

--- Direct3D11Stuff/Window.cpp
+++ Direct3D11Stuff/Window.cpp
@@ -110,6 +110,24 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	case WM_CLOSE:
 		PostQuitMessage(0);
 		return 0;
+
+	//Keyboard messages
+	case WM_KILLFOCUS:
+		kbd.ClearState();
+		break;
+	//Handle system keys too (ALT)
+	case WM_KEYDOWN:
+	case WM_SYSKEYDOWN:
+		if(!(lParam & 0x40000000) || kbd.AutorepeatIsEnabled())
+			kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
+		break;
+	case WM_KEYUP:
+	case WM_SYSKEYUP:
+		kbd.OnKeyReleased(static_cast<unsigned char>(wParam));
+		break;
+	case WM_CHAR:
+		kbd.OnChar(static_cast<unsigned char>(wParam));
+		break;
 	}
 
 	return DefWindowProc(hWnd, msg, wParam, lParam);
