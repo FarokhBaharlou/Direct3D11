8b7b9c8cf88d90027bb6d2083021587546004a7c
Farokh <farokhbahalou@yahoo.com>
2021-12-07 01:06:56 -0500

    Added 3d3 exception handling (d3d hresult handling)

--- Direct3D11Stuff/Window.cpp
+++ Direct3D11Stuff/Window.cpp
@@ -83,7 +83,7 @@ void Window::SetTitle(const std::string& title)
 		throw FWND_LAST_EXCEPT();
 }
 
-std::optional<int> Window::ProcessMessage()
+std::optional<int> Window::ProcessMessage() noexcept
 {
 	MSG msg;
 
@@ -101,6 +101,10 @@ std::optional<int> Window::ProcessMessage()
 
 Graphics& Window::Gfx()
 {
+	if (!pGfx)
+	{
+		throw FWND_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -235,28 +239,11 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-Window::Exception::Exception(int line, const char* file, HRESULT hr) noexcept : FarokhException{line, file}, hr{hr}
-{
-}
-
-const char* Window::Exception::what() const noexcept
-{
-	std::ostringstream oss;
-	oss << GetType() << std::endl << "[Error Code]" << GetErrorCode() << std::endl << "[Discription]" << GetErrorString() << std::endl << GetOriginString();
-	whatBuffer = oss.str();
-	return whatBuffer.c_str();
-}
-
-const char* Window::Exception::GetType() const noexcept
-{
-	return "Window Exception";
-}
-
 std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
 	char* pMsgBuf = nullptr;
 	// windows will allocate memory for err string and make our pointer point to it
-	DWORD nMsgLen = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPSTR>(&pMsgBuf), 0, nullptr);
+	const DWORD nMsgLen = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPSTR>(&pMsgBuf), 0, nullptr);
 
 	if (nMsgLen == 0)
 		return "Unidentified error code";
@@ -265,12 +252,40 @@ std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 	return errorString;
 }
 
-HRESULT Window::Exception::GetErrorCode() const noexcept
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	:
+	Exception(line, file),
+	hr(hr)
+{}
+
+const char* Window::HrException::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Window::HrException::GetType() const noexcept
+{
+	return "Chili Window Exception";
+}
+
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-std::string Window::Exception::GetErrorString() const noexcept
+std::string Window::HrException::GetErrorDescription() const noexcept
 {
-	return TranslateErrorCode(hr);
+	return Exception::TranslateErrorCode(hr);
+}
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "Farokh Window Exception [No Graphics]";
 }
--- Direct3D11Stuff/Window.h
+++ Direct3D11Stuff/Window.h
@@ -13,15 +13,26 @@ class Window
 public:
 	class Exception : public FarokhException
 	{
+		using FarokhException::FarokhException;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
+	};
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
 	};
 private:
 	//Singleton that manages registration of our window
@@ -45,7 +56,7 @@ public:
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
 	void SetTitle(const std::string& title);
-	static std::optional<int> ProcessMessage();
+	static std::optional<int> ProcessMessage() noexcept;
 	Graphics& Gfx();
 private:
 	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
@@ -62,5 +73,6 @@ private:
 };
 
 // Exception helper macro
-#define FWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr )
-#define FWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
+#define FWND_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
+#define FWND_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
+#define FWND_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
